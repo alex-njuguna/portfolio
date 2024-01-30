db64b766974e44ad26524505c595f6dd4751e133
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-30 03:41:57 +0300

    setting up contact form

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+
 export default function ContactMe() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [topic, setTopic] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formMessage = `${firstName} ${lastName} (${email}) wants to talk about ${topic}. Message: ${message}`;
+    window.open(
+      `https://wa.me/${phoneNumber}?text=${encodeURIComponent(formMessage)}`,
+      "_blank"
+    );
+  };
+
   return (
     <section id="Contact" className="contact--section">
       <div>
         <p className="sub--title">Get In Touch</p>
         <h2>Contact Me</h2>
-        <p className="text-lg">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In, odit.
-        </p>
+        <p className="text-lg">Fill The Form Below To Reach Out</p>
       </div>
-      <form className="contact--form--container">
+      <form className="contact--form--container" onSubmit={handleSubmit}>
         <div className="container">
           <label htmlFor="first-name" className="contact--label">
             <span className="text-md">First Name</span>
@@ -18,6 +35,8 @@ export default function ContactMe() {
               name="first-name"
               id="first-name"
               required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </label>
           <label htmlFor="last-name" className="contact--label">
@@ -28,6 +47,8 @@ export default function ContactMe() {
               name="last-name"
               id="last-name"
               required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
             />
           </label>
           <label htmlFor="email" className="contact--label">
@@ -38,6 +59,8 @@ export default function ContactMe() {
               name="email"
               id="email"
               required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </label>
           <label htmlFor="phone-number" className="contact--label">
@@ -47,17 +70,26 @@ export default function ContactMe() {
               className="contact--input text-md"
               name="phone-number"
               id="phone-number"
+              placeholder="+254 722 111 111"
               required
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
             />
           </label>
         </div>
         <label htmlFor="choode-topic" className="contact--label">
           <span className="text-md">Choose a topic</span>
-          <select id="choose-topic" className="contact--input text-md">
+          <select
+            id="choose-topic"
+            className="contact--input text-md"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          >
             <option>Select One...</option>
-            <option>Item 1</option>
-            <option>Item 2</option>
-            <option>Item 3</option>
+            <option>Development</option>
+            <option>Testing and QA</option>
+            <option>Consultancy</option>
+            <option>Invite me for an interview</option>
           </select>
         </label>
         <label htmlFor="message" className="contact--label">
@@ -67,14 +99,19 @@ export default function ContactMe() {
             id="message"
             rows="8"
             placeholder="Type your message..."
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
         </label>
-        <label htmlFor="checkboc" className="checkbox--label">
+        <label htmlFor="checkbox" className="checkbox--label">
           <input type="checkbox" required name="checkbox" id="checkbox" />
           <span className="text-sm">I accept the terms</span>
         </label>
         <div>
-          <button className="btn btn-primary contact--form--btn">Submit</button>
+          <button type="submit" className="btn btn-primary contact--form--btn">
+            Submit
+          </button>
         </div>
       </form>
     </section>
