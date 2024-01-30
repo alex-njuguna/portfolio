6b261f5ea4737538d1338099bdfa57e8028dba3e
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-30 18:33:37 +0300

    hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,20 +1,25 @@
+import { Link } from "react-scroll";
+
 export default function HeroSection() {
   return (
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hey, I'm John</p>
+          <p className="section--title">Hey, I'm Alex</p>
           <h1 className="hero--section--title">
             <span className="hero--section-title--color">Full Stack</span>{" "}
             <br />
             Developer
           </h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
+            Versatile developer crafting efficient solutions with creativity and
+            precision. Team player.
           </p>
         </div>
-        <button className="btn btn-primary">Get In Touch</button>
+        <br />
+        <Link to="Contact">
+          <button className="btn btn-primary">Get In Touch</button>
+        </Link>
       </div>
       <div className="hero--section--img">
         <img src="./img/hero_img.png" alt="Hero Section" />
